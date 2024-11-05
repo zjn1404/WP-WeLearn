b283bd9cad03edca3464cc84d72bd8d53f2ffa45
FAT29 <lqm550@gmail.com>
2024-11-06 02:05:09 +0700

    Update Announcment

--- WeLearnApp/desktop-app/TutorApp/ViewModels/LoginViewModel.cs
+++ WeLearnApp/desktop-app/TutorApp/ViewModels/LoginViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TutorApp.Models.ForAPI.Request;
 using TutorApp.Models.ForAPI.Response;
@@ -38,6 +39,48 @@ namespace TutorApp.ViewModels
             {
                 throw new Exception($"Login error: {ex.Message}");
             }
+        }
+
+        public string ValidateInput(LoginRequest loginRequest)
+        {
+            if (string.IsNullOrEmpty(loginRequest.username) ||
+                string.IsNullOrEmpty(loginRequest.password) )
+            {
+                return "Please fill in all the information";
+            }
+
+            if (!IsValidUser(loginRequest.username))
+            {
+                return "User must be at least 5 characters";
+            }
+
+
+            if (!IsValidPassword(loginRequest.password))
+            {
+                return "The password must be at least 8 characters long and include an uppercase letter, a number, and a special character!";
+            }
+
+            return null;
+        }
+
+        private bool IsValidUser(string user)
+        {
+            return user.Length >= 5;
+        }
+
+
+        private bool IsValidPassword(string password)
+        {
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSpecialChar = new Regex(@"[!@#$%^&*(),.?""':{}|<>]+");
+
+            return password.Length >= 8 &&
+                   hasNumber.IsMatch(password) &&
+                   hasUpperChar.IsMatch(password) &&
+                   hasLowerChar.IsMatch(password) &&
+                   hasSpecialChar.IsMatch(password);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
