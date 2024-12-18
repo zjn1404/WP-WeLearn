43606524b72a733e7e7855d53391d5d353f86475
zjn1404 <nqt460@gmail.com>
2024-12-18 23:43:48 +0700

    Finish my ordered page

--- WeLearnApp/desktop-app/TutorApp/Views/HomePage/MyOrderedSessionPage.xaml.cs
+++ WeLearnApp/desktop-app/TutorApp/Views/HomePage/MyOrderedSessionPage.xaml.cs
@@ -1,6 +1,11 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using TutorApp.Models.ForAPI.Response;
 using TutorApp.Services;
 using TutorApp.Services.Interfaces;
 using TutorApp.Services.Interfaces.ForAPI;
@@ -19,6 +24,44 @@ namespace TutorApp.Views.HomePage
             var navigationService = App.Current.Services.GetService<INavigationService>();
             ViewModel = new MyOrderedSessionViewModel(learningSessionService);
             this.DataContext = ViewModel;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private LearningSessionResponse _selectedSession;
+        public LearningSessionResponse SelectedSession
+        {
+            get => _selectedSession;
+            set
+            {
+                _selectedSession = value;
+                OnPropertyChanged(nameof(SelectedSession));
+            }
+        }
+
+        private async void OnSessionCardClicked(object sender, ItemClickEventArgs e)
+        {
+            Debug.WriteLine($"Clicked item: {e.ClickedItem}");
+            if (e.ClickedItem is OrderResponse order)
+            {
+                var session = order.orderDetail.learningSession;
+
+                SelectedSession = session;
+                SelectedSession.Tutor = order.tutor;
+                SessionDetailsDialog.DataContext = null;
+                SessionDetailsDialog.DataContext = this;
+                await SessionDetailsDialog.ShowAsync();
+            }
+        }
+
+        private async void OnProceedToJoinClassroom(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            throw new NotImplementedException();
         }
     }
 }
