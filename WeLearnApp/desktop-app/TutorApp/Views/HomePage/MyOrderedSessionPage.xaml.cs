5c13db5fd89732713a23e948fef7d44f22d31551
22120118 <Shungisme@gmail.com>
2024-12-29 21:31:47 +0700

    Integrate api call video

--- WeLearnApp/desktop-app/TutorApp/Views/HomePage/MyOrderedSessionPage.xaml.cs
+++ WeLearnApp/desktop-app/TutorApp/Views/HomePage/MyOrderedSessionPage.xaml.cs
@@ -15,11 +15,14 @@ namespace TutorApp.Views.HomePage
 {
     public sealed partial class MyOrderedSessionPage : Page
     {
+        private readonly IVideoCallService _videoCallService;
+
         public MyOrderedSessionViewModel ViewModel { get; }
 
         public MyOrderedSessionPage()
         {
             this.InitializeComponent();
+            _videoCallService = ((App)Application.Current).Services.GetRequiredService<IVideoCallService>();
             var learningSessionService = App.Current.Services.GetService<ILearningSessionService>();
             var navigationService = App.Current.Services.GetService<INavigationService>();
             ViewModel = new MyOrderedSessionViewModel(learningSessionService);
@@ -61,7 +64,42 @@ namespace TutorApp.Views.HomePage
 
         private async void OnProceedToJoinClassroom(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string learningSessionId = SelectedSession?.Id.ToString() ?? "";
+
+
+                string roomUrl = await _videoCallService.JoinRoom(learningSessionId);
+                Debug.WriteLine("paymentUrl", roomUrl);
+                if (!string.IsNullOrEmpty(roomUrl))
+                {
+                    var uri = new Uri(roomUrl);
+                    Debug.WriteLine("uri", uri);
+                    await Windows.System.Launcher.LaunchUriAsync(uri);
+                }
+                else
+                {
+                    ContentDialog errorDialog = new ContentDialog
+                    {
+                        Title = "Joining Room Error",
+                        Content = "Unable to join the room. Please try again.",
+                        CloseButtonText = "Close",
+                        XamlRoot = this.XamlRoot
+                    };
+                    await errorDialog.ShowAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ContentDialog errorDialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"An error occurred: {ex.Message}",
+                    CloseButtonText = "Close",
+                    XamlRoot = this.XamlRoot
+                };
+                await errorDialog.ShowAsync();
+            }
         }
     }
 }
