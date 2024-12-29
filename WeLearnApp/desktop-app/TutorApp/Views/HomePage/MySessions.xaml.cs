939369ff5f9a27733ea18e9db113985a1f9f8ded
22120118 <Shungisme@gmail.com>
2024-12-29 22:01:12 +0700

    Fix dialog tutor's sessions

--- WeLearnApp/desktop-app/TutorApp/Views/HomePage/MySessions.xaml.cs
+++ WeLearnApp/desktop-app/TutorApp/Views/HomePage/MySessions.xaml.cs
@@ -8,9 +8,14 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TutorApp.Models;
+using TutorApp.Models.ForAPI.Response;
+using TutorApp.Services;
 using TutorApp.Services.Interfaces;
 using TutorApp.Services.Interfaces.ForAPI;
 using TutorApp.ViewModels;
@@ -27,16 +32,90 @@ namespace TutorApp.Views.HomePage
     /// </summary>
     public sealed partial class MySessions : Page
     {
+        private readonly IVideoCallService _videoCallService;
         public TutorLearningSessionViewModel ViewModel { get; }
         public ILearningSessionService learningSessionService;
 
         public MySessions()
         {
             this.InitializeComponent();
+            _videoCallService = ((App)Application.Current).Services.GetRequiredService<IVideoCallService>();
             var navigationService = App.Current.Services.GetService<INavigationService>();
             learningSessionService = App.Current.Services.GetService<ILearningSessionService>();
             ViewModel = new TutorLearningSessionViewModel(learningSessionService);
             DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private LearningSession _selectedSession;
+        public LearningSession SelectedSession
+        {
+            get => _selectedSession;
+            set
+            {
+                _selectedSession = value;
+                OnPropertyChanged(nameof(SelectedSession));
+            }
+        }
+
+        private async void OnSessionCardClicked(object sender, ItemClickEventArgs e)
+        {
+            Debug.WriteLine($"Clicked item: {e.ClickedItem}");
+            if (e.ClickedItem is LearningSession session)
+            {
+                SelectedSession = session;
+                SessionDetailsDialog.DataContext = null;
+                SessionDetailsDialog.DataContext = this;
+                await SessionDetailsDialog.ShowAsync();
+            }
+        }
+
+
+        private async void OnProceedToJoinClassroom(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            try
+            {
+                string learningSessionId = SelectedSession?.Id.ToString() ?? "";
+
+
+                string roomUrl = await _videoCallService.JoinRoom(learningSessionId);
+                Debug.WriteLine("paymentUrl", roomUrl);
+                if (!string.IsNullOrEmpty(roomUrl))
+                {
+                    var uri = new Uri(roomUrl);
+                    Debug.WriteLine("uri", uri);
+                    await Windows.System.Launcher.LaunchUriAsync(uri);
+                }
+                else
+                {
+                    ContentDialog errorDialog = new ContentDialog
+                    {
+                        Title = "Joining Room Error",
+                        Content = "Unable to join the room. Please try again.",
+                        CloseButtonText = "Close",
+                        XamlRoot = this.XamlRoot
+                    };
+                    await errorDialog.ShowAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ContentDialog errorDialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = $"An error occurred: {ex.Message}",
+                    CloseButtonText = "Close",
+                    XamlRoot = this.XamlRoot
+                };
+                await errorDialog.ShowAsync();
+            }
         }
     }
 }
